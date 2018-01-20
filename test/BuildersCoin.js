c29372a2cc3fea7e4f929a1ca7d6d9983afb2c1b
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2018-01-20 17:19:19 +0300

    chore(test): add 'standard token' test

--- test/BuildersCoin.js
+++ test/BuildersCoin.js
@@ -1,11 +1,15 @@
 import basic from './BuildersCoin/basic';
 import mintable from './BuildersCoin/mintable';
+import standard from './BuildersCoin/standard';
 
 const BuildersCoin = artifacts.require('../contracts/Tokens/BuildersCoin.sol');
 
 contract('BuildersCoin', function (accounts) {
   describe('Basic Token', function () {
     basic(BuildersCoin, accounts);
+  });
+  describe('Standard Token', function () {
+    standard(BuildersCoin, accounts);
   });
   describe('Mintable Token', function () {
     mintable(BuildersCoin, accounts);
