fdd9c50c6bba9c5b2a37910992577fe4544657af
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2018-01-25 01:46:36 +0300

    chore(Configurator): add directMintAgent setter

--- contracts/Configurator.sol
+++ contracts/Configurator.sol
@@ -7,9 +7,7 @@ import './Presale.sol';
 contract Configurator is Ownable {
 
   BuildersCoin public token;
-
   Presale public presale;
-
 
   function deploy() public onlyOwner {
 
@@ -21,16 +19,20 @@ contract Configurator is Ownable {
     presale.setSoftcap(1000000000000000000); // 1 ETH
     presale.setHardcap(10000000000000000000); // 10 ETH
     presale.setStart(1516791600); // Jan 24 2018 14:00:00 GMT+0300
-    presale.setDuration(30);
+    presale.setDuration(30); // 30 days
     presale.setWallet(0xd89626E2c4218281Ad0Fc5F22AE52dC0FF39DDC4); // my test wallet in Ropsten
-
     presale.setToken(token);
+
     token.setSaleAgent(presale);
 
     address manager = 0xf62158b03Edbdb92a12c64E4D8873195AC71aF6A; // my test wallet in Ropsten
+
+    // remove this two lines if directMintAgent == manager
+    presale.setDirectMintLimit(1000000000000000000000000); // 1 000 000 BLD
+    presale.setDirectMintAgent(0xf62158b03Edbdb92a12c64E4D8873195AC71aF6A);
+
     token.transferOwnership(manager);
     presale.transferOwnership(manager);
   }
-
 
 }
