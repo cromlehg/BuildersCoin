0d13a90380cb21b88b9cf1635216b28bf4f64165
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2018-02-21 18:16:29 +0300

    chore(project): update configs

--- truffle.js
+++ truffle.js
@@ -1,11 +1,20 @@
 require('babel-register');
 require('babel-polyfill');
+
 module.exports = {
   networks: {
-    ganache: {
+    test: {
       host: 'localhost',
       port: 8545,
-      network_id: '*'
+      network_id: '*',
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
 };
